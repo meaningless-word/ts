3f9382913b7291b516faf488b0595b9e66f838eb
meaningless-word <grisly@mail.ru>
2024-04-07 23:35:18 +0700

    передача через props.children

---
+++ src/components/Alerts.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import Alert from "react-bootstrap/Alert";
+
+function Alerts(props) {
+  return <Alert variant={"danger"}>{props.children}</Alert>;
+}
+
+export default Alerts;
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,16 +1,18 @@
 import * as React from "react";
 
 import Countries from "./Countries";
+import Alerts from "./Alerts";
 
 import "../styles/Main.css";
 
 function Main() {
-
-    return (
-        <main>
-            <Countries />
-        </main>
-    );
+  let alertText = <b>Текст для алерта, переданный контентом компонента</b>;
+  return (
+    <main>
+      <Alerts>{alertText}</Alerts>
+      <Countries />
+    </main>
+  );
 }
 
 export default Main;
